b7db5b78d5ded62838cdf6d9addeaed487f35031
ArtemTrue <lobzik45@gmail.com>
2016-04-01 17:14:44 +0600

    Java code cleaned up.

--- app/src/main/java/com/artemtrue/justjava/MainActivity.java
+++ app/src/main/java/com/artemtrue/justjava/MainActivity.java
@@ -21,7 +21,6 @@ public class MainActivity extends ActionBarActivity {
     String addWhippedCream = "Add whipped cream? ";
     String addChocolate = "Add Chocolate? ";
     int price = 0;
-    String hasWhippedCream;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,33 +28,16 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /**
-     * This method is called when the order button is clicked.
-     */
-
-    public void submitOrder(View view) {
-        /** по клику на кнопку Order вызывается этот метод, который вызывает
-         * метод display в котором в текствью пихается текст и, в итоге, отображается 1 */
-        int price = calculatePrice();
-
-        //Берем введенное имя и выводим его в строке заказа
-        EditText nameInputField = (EditText) findViewById(R.id.name_input_field);
-        enteredName = "Name: " + nameInputField.getText().toString();
-
-        String priceMessage = createOrderSummary(price);
-        displayMessage(priceMessage);
-    }
-
+    /** метод для увеличения количества чашек */
     public void increaseValue(View view) {
 
-        //display(quantity + 1);
         quantity = quantity + 1;
         displayQuantity(quantity);
     }
 
+    /** метод для уменьшения количества чашек */
     public void decreaseValue(View view) {
 
-        //display(quantity - 1);
         quantity = quantity - 1;
         if(quantity <= 0)
         {
@@ -64,62 +46,21 @@ public class MainActivity extends ActionBarActivity {
         displayQuantity(quantity);
     }
 
-    /**
-     * This method displays the given quantity value on the screen.
-     */
+    /** вывод на экран количества чашек для заказа */
     private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
 
-    /* private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    } */
-
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
-    }
-
+    /** расчет стоимости заказа (кол-во * цену за чашку) */
     private int calculatePrice() {
         price = quantity * 5;
         return price;
     }
 
-
-    public String createOrderSummary(int price) {
-
-        CheckBox whippedCream = (CheckBox) findViewById(R.id.whip_checkbox);
-        CheckBox chocolate = (CheckBox) findViewById(R.id.choco_checkbox);
-
-        boolean whippedCreamChecked = whippedCream.isChecked();
-        boolean chocolateChecked = chocolate.isChecked();
-
-        //делает вывод whipped cream true, если стоит чек
-        if(whippedCream.isChecked())
-        {
-            String priceMessage = enteredName + "\n"
-                    + addWhippedCream + whippedCreamChecked + "\n"
-                    + addChocolate + chocolateChecked + "\n"
-                    + kolichestvo + quantity +
-                    "\nTotal: $" + price +
-                    "\n" + thanks;
-            return priceMessage;
-        }
-
-        //делает вывод chocolate true, если стоит чек
-        if(chocolate.isChecked())
-        {
-            String priceMessage = enteredName + "\n"
-                    + addWhippedCream + whippedCreamChecked + "\n"
-                    + addChocolate + chocolateChecked + "\n"
-                    + kolichestvo + quantity +
-                    "\nTotal: $" + price +
-                    "\n" + thanks;
-            return priceMessage;
-        }
+    /** метод формирует саммари заказа */
+    public String createOrderSummary(int price, boolean whippedCreamChecked, boolean chocolateChecked, String enteredName) {
 
         //выводит для whipped cream и chocolate false, если не стоят чеки
         String priceMessage = enteredName + "\n"
@@ -130,12 +71,32 @@ public class MainActivity extends ActionBarActivity {
                 "\n" + thanks;
 
         return priceMessage;
-
-
-        /**По курсу Udacity:
-         *
-         * boolean hasWhippedCream = whippedCream.isChecked();
-         *String priceMessage = name + "\n" + addWhippedCream + whippedCream + "\n" + kolichestvo + quantity + "\nTotal: $" + price + "\n" + thanks;
-         * */
     }
+
+    public void submitOrder(View view) {
+        /** по клику на кнопку Order вызывается этот метод, который вызывает
+         * метод display в котором в текствью пихается текст и, в итоге, отображается 1 */
+        int price = calculatePrice();
+
+        CheckBox whippedCream = (CheckBox) findViewById(R.id.whip_checkbox);
+        CheckBox chocolate = (CheckBox) findViewById(R.id.choco_checkbox);
+
+        //берем состояния чекбоксов и кладём их в переменные
+        boolean whippedCreamChecked = whippedCream.isChecked();
+        boolean chocolateChecked = chocolate.isChecked();
+
+        //Берем введенное имя и выводим его в строке заказа
+        EditText nameInputField = (EditText) findViewById(R.id.name_input_field);
+        enteredName = "Name: " + nameInputField.getText().toString();
+
+        String priceMessage = createOrderSummary(price, whippedCreamChecked, chocolateChecked, enteredName);
+        displayMessage(priceMessage);
+    }
+
+    /** вывод всего заказа на экран */
+    private void displayMessage(String message) {
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
+    }
+
 }
